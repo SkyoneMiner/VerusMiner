5f85e10587ebc0a540c7b593fd1559720cf9e230
SkyoneMiner <130772654+SkyoneMiner@users.noreply.github.com>
2024-11-28 23:01:52 +0700

    Update userland_Install.sh

--- userland_Install.sh
+++ userland_Install.sh
@@ -7,4 +7,4 @@ sudo apt autoclean
 wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
 
 tar -xvf hellminer_linux64.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RLa5js7sJpYTQtFr6hFnqQVqMLZ9xCMPP8.vsm1 -p x -t 
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RLa5js7sJpYTQtFr6hFnqQVqMLZ9xCMPP8.vsm1 -p x 
